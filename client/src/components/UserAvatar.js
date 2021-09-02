dee297d3d75cd3a0b8f414ee5aa0e8cf27ef0514
m365i <qanda280@gmail.com>
2021-09-02 19:13:03 +0300

    remove chat loading indicator when list is smaller then the screen

--- client/src/components/UserAvatar.js
+++ client/src/components/UserAvatar.js
@@ -3,7 +3,6 @@ import {Avatar} from '@material-ui/core'
 import React from 'react'
 
 function UserAvatar({userId, size, circle}) {
-	console.log(userId, size)
 	return (
 		<Avatar sizes={size} style={{width: size, height: size}} alt="user image" variant={circle ? 'circle' : 'square'}  src={process.env.REACT_APP_SERVER_URL + '/api/profile/img/get_img/' + userId}>
 			<img height={size} width={size} src={'https://avatars.dicebear.com/api/bottts/' + userId + '.svg'} alt="user image" />
